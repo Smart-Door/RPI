62270bb1bce42a3f0d824e7d669dfec22509343f
jesperjulle <jesper1998@live.dk>
2023-11-30 01:39:53 +0100

    Update SystemState.cpp

--- SystemState.cpp
+++ SystemState.cpp
@@ -21,36 +21,3 @@ void SystemState::doorIsClosed() {
 }
 
 
-// bool SystemState::attemptUserLogin(const std::string& password) {
-
-//     //if system is locked message
-//     if (isSystemLocked.load()) {
-//         std::cout << "System is locked. Please enter admin password to unlock." << std::endl;
-//         return false;
-//     }
-
-//     //checks userpassword and increments failed password counter.
-//     //Locks with 3 failed
-//     if (!checkUserPassword(password)) {
-//         failedPasswordAttempts++;
-//         if (failedPasswordAttempts.load() >= 3) {
-//             isSystemLocked = true;
-//             std::cout << "System locked due to too many failed attempts." << std::endl;
-//         }
-//         return false;
-//     }
-
-//     failedPasswordAttempts = 0; //resets the failed password counter
-//     return true; //successful login
-// }
-
-// //unlock the system when in password attempt locked state
-// void SystemState::unlockSystem(const std::string& adminPassword) {
-//     if (checkAdminPassword(adminPassword)) {
-//         isSystemLocked = false;
-//         failedPasswordAttempts = 0;
-//         std::cout << "System unlocked." << std::endl;
-//     } else {
-//         std::cout << "Incorrect admin password." << std::endl;
-//     }
-// }
